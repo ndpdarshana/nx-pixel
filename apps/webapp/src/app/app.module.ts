957a35596aeb87b5730951bc1684af09555d6d2a
Prabhath Darshana <ndp.mymail@gmail.com>
2020-05-17 10:04:44 +1000

    Empty new component

--- apps/webapp/src/app/app.module.ts
+++ apps/webapp/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { ToolbarComponent } from './common/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ToolbarComponent],
   imports: [
     BrowserModule, 
     BrowserAnimationsModule,
