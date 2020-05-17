528b100d09f96c63755d03c4f2861fc799b764ee
Prabhath Darshana <ndp.mymail@gmail.com>
2020-05-17 10:15:54 +1000

    Initial uploading of toolbar

--- apps/webapp/src/app/app.module.ts
+++ apps/webapp/src/app/app.module.ts
@@ -3,7 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/**
+ * Material components
+ */
 import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 import { ToolbarComponent } from './common/toolbar/toolbar.component';
 
 @NgModule({
@@ -11,7 +18,9 @@ import { ToolbarComponent } from './common/toolbar/toolbar.component';
   imports: [
     BrowserModule, 
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
