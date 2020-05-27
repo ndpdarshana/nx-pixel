c89ea456a917c01f808ed07fbea59e4ea9f3c444
Prabhath Darshana <ndp.mymail@gmail.com>
2020-05-27 11:09:05 +1000

    user Data table creation

--- apps/webapp/src/app/modules/hr/hr.module.ts
+++ apps/webapp/src/app/modules/hr/hr.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+
 import { HrComponent } from '../../modules/hr/hr.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { TimecardsComponent } from './employees/timecards/timecards.component';
 import { AdministrationComponent } from './administration/administration.component';
 import { UsersComponent } from './administration/users/users.component';
 import { MaintenanceComponent } from './administration/users/maintenance/maintenance.component';
-
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { RouterModule } from '@angular/router';
+import { UserTableComponent } from './administration/users/user-table/user-table.component';
 
 @NgModule({
   declarations: [
@@ -21,14 +25,18 @@ import { RouterModule } from '@angular/router';
     TimecardsComponent,
     AdministrationComponent,
     UsersComponent,
-    MaintenanceComponent
+    MaintenanceComponent,
+    UserTableComponent
   ],
   imports: [
     CommonModule,
     MatCardModule,
     MatDividerModule,
+    MatTableModule,
     FlexLayoutModule,
-    RouterModule
+    RouterModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   exports:[
     HrComponent
